3939e45e790afb236c9be1d051ab7ba611800c7e
unknown <FPE@965PDS3-009.aeg8.edu>
2015-03-09 14:16:16 +0100

    ejer formularios

--- Ejers_JavaScript/Formularios/jsFormularios.js
+++ Ejers_JavaScript/Formularios/jsFormularios.js
@@ -1,4 +1,24 @@
-email = document.getElementById("registro_email").value;
+var nombre = document.getElementById("registro_nombre");
+
+var apellido = document.getElementById("registro_apellido");
+
+var email = document.getElementById("registro_email").value;
 if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
-  return false;
+    return false;
+}
+
+var pass = document.getElementById("registro_password");
+if( pass.length < 6 ) {
+    return false;
+}
+else{
+    if(){
+        return false;
+    }
+
+}
+
+var comentarios = documet.getElementById("registro_comentarios");
+if (comentarios.length > 50){
+    return false;
 }
---
+++ Ejers_JavaScript/Formularios/servidor.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Ejercicio 21 - Validación de formularios</title>
+    <link rel="stylesheet" type="text/css" href="stylesFormularios.css" />
+    <script type="text/javascript" src="jsFormularios.js"></script>
+</head>
+
+<body>
+    <div>El formulario a sido enviado correctamente</div>
+</body>
+</html>
